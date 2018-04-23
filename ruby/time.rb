aa1812c06ae4c3973c85b923fc5b48e7b762afc0
tang05709 <tangyong0425@sina.com>
2018-04-23 11:47:55 +0800

    Create time.rb

---
+++ ruby/time.rb
@@ -0,0 +1,18 @@
+  ####### 计算剩余报价天数 ####### 
+  def get_last_validity(created_at, last_validity)
+    res = '不限'
+    if last_validity.blank?
+      return res
+    end
+    # 已发布天数
+    time_diff = (Time.now.to_i - created_at.to_i) / (24 * 60 * 60)
+    # 剩余天数
+    left_day = last_validity.to_i - time_diff
+    if left_day > 0
+      res = "#{left_day}后过期"
+    else
+      res = "已过期"
+    end
+    return res
+  end
+  ####### 计算剩余报价天数 #######
