d906d2b9fd3e9652842c27c20884f91164e31254
tang05709 <tangyong0425@sina.com>
2018-05-15 17:27:28 +0800

    Update commonly_use.rb

--- ruby/commonly_use.rb
+++ ruby/commonly_use.rb
@@ -11,3 +11,12 @@ end
 article = Article.where(condition).where(category_id: category_id).order(order).first
 # 上一篇 下一篇
       
+# 电话号码中间使用*号代替
+def  replace_phone(phone)
+  # 电话号码假定11位
+  # 获取电话号码的4-7位
+  ready_replace = phone[3, 6]
+  phone[ready_replace] = '****'
+  phone
+end
+# 电话号码中间使用*号代替
