7273ef3e78f8e6d09c1609c9cd5c9ddb981992b7
tang05709 <tangyong0425@sina.com>
2018-05-15 14:50:25 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/********es6 随机数, 使用了eslint, 所以计算时括号要明确*********/
+const initX = 100
+const initY = 500
+// (Math.random() * (maxWidth - 20 + 1) | 0) + 20
+const rule = (initX - initY) + 1
+const leftRand = (Math.random() * rule) + initY
+/********es6 随机数, 使用了eslint, 所以计算时括号要明确*********/
