ce0ea58d7aa4c1d003349820aa06d1e717c7c85d
tang05709 <tangyong0425@sina.com>
2018-06-09 14:08:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,11 @@ const initY = 500
 const rule = (initX - initY) + 1
 const leftRand = (Math.random() * rule) + initY
 /********es6 随机数, 使用了eslint, 所以计算时括号要明确*********/
+
+/*****截取并替换电话号码*****/
+var replacePhone = function (phone) {
+  var replaceStr = phone.substring(3, 7)
+  var result = phone.replace(replaceStr, '****')
+  return result
+}
+/*****截取并替换电话号码*****/
