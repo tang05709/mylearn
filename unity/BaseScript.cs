e4afcf91a363a9b9c113d1354e7aa61b1f2cf3ce
tang05709 <tangyong0425@sina.com>
2019-01-18 17:55:18 +0800

    Update BaseScript.cs

--- unity/BaseScript.cs
+++ unity/BaseScript.cs
@@ -7,3 +7,28 @@ if (GUILayout.Button("创建立方体", GUILayout.Height(50)))
     obj.name = "Cube";
     obj.transform.position = new Vector3(0, 5f, 0);
 }
+
+if (GUILayout.Button("向左移动物体"))
+{
+    cube.transform.Translate(new Vector3(-5f, 0, 0));
+}
+if (GUILayout.Button("向右移动物体"))
+{
+    cube.transform.position = cube.transform.position + new Vector3(5f, 0, 0);
+}
+if (GUILayout.Button("放大物体"))
+{
+    cube.transform.localScale *= 2f;
+}
+if (GUILayout.Button("缩小物体"))
+{
+    cube.transform.localScale /= 2f;
+}
+if (GUILayout.Button("旋转物体"))
+{
+    cube.transform.Rotate(new Vector3(0, 10f, 0));
+}
+if (GUILayout.Button("围绕球体旋转物体"))
+{
+    cube.transform.RotateAround(cylinder.transform.position, Vector3.up, 10);
+}
