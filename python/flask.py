2dc1ce96dee3d6ed55f5906c251aecaafa675794
tang05709 <tangyong0425@sina.com>
2019-02-17 17:23:08 +0800

    Update flask.py

--- python/flask.py
+++ python/flask.py
@@ -3,4 +3,6 @@
 parent_id = SelectField('parent_id', validators = [InputRequired()])
 #view
 form.parent_id.choices = [('','请选择'), (0, '顶级栏目')] + [(g.id, g.name) for g in find_taxons()]
-  
+# 或者
+form.parent_id.choices.insert(0, ('', '请选择'))
+form.parent_id.choices.insert(1, (0, '顶级栏目'))
