7b6c28d91fe0476b83b6d581453dcc491cc11f19
tang05709 <tangyong0425@sina.com>
2019-12-20 16:25:28 +0800

    Update other.php

--- php/other.php
+++ php/other.php
@@ -11,30 +11,37 @@ list($b,$a)=array($a,$b);
 var_dump($a,$b);
 
 /**
-     * 将一个时间段分成n份
-     * start_at、end_at 开始和结束时间，需为时间戳
-     * count 份数
-     */
-    private function makeTimeCut($start_at, $end_at, $count) {
-        $count = intval($count);
-        // 计算分段数量
-        $parts = ($end_at - $start_at) / $count;
-        // 是否除尽
-        $remainder = ($end_at - $start_at) % $count;
-        if($remainder > 0) {
-            // 重新计算分段数量
-            $parts = ($end_at - $start_at - $remainder) / $count;
-        }
-        $times = [];
-        for($i = 0; $i < $count; $i++) {
-            $start = $start_at + $parts * $i;
-            $end = $start + $parts;
-            $data = [
-                'start_at' => $start,
-                'end_at' => $end,
-            ];
-            $times[] = $data; 
-        }
-
-        return $times;
+ * 将一个时间段分成n份
+ * start_at、end_at 开始和结束时间，需为时间戳
+ * count 份数
+*/
+private function makeTimeCut($start_at, $end_at, $count) {
+    $count = intval($count);
+    // 计算分段数量
+    $parts = ($end_at - $start_at) / $count;
+    // 是否除尽
+    $remainder = ($end_at - $start_at) % $count;
+    if($remainder > 0) {
+        // 重新计算分段数量
+        $parts = ($end_at - $start_at - $remainder) / $count;
     }
+    $times = [];
+    for($i = 0; $i < $count; $i++) {
+        $start = $start_at + $parts * $i;
+        $end = $start + $parts;
+        $data = [
+            'start_at' => $start,
+            'end_at' => $end,
+        ];
+        $times[] = $data; 
+    }
+
+    return $times;
+}
+            
+            
+  public static function generateSn()
+  {
+       $SN = date("YmdHis") . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9);
+       return $SN;
+  }
